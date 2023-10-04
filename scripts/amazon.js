01d0c62ebd6213053b6524784398266da877e006
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-04 22:02:30 +0100

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,6 +80,13 @@ if(matchingItem){
             quantity: 1
         });
 }
+let cartQuantity = 0
+
+cart.forEach((item)=>{
+    cartQuantity += item.quantity
+})
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
 
         console.log(cart); 
     })
